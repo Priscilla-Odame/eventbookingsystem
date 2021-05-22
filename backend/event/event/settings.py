8a9daf887bfea567408acf44098f1cebddb1bf01
Priscilla-Odame <priscillaodame5@gmail.com>
2021-05-22 18:02:35 +0000

    Store env variables and implement git ignore

--- backend/event/event/settings.py
+++ backend/event/event/settings.py
@@ -11,19 +11,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+import environ
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nu)ll!%p0ojim&tz^$cdu0&qtut@qb%*zzbyb%^0sw90eh0n^q'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 

2defb851ee012859fd084a7179cbf5506ddfa768
Priscilla-Odame <priscillaodame5@gmail.com>
2021-05-22 18:09:51 +0000

    create user model

--- backend/event/app/admin.py
+++ backend/event/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','firstname', 'lastname','email','password','date_of_birth')
+
+
+admin.site.register(User,UserAdmin)
